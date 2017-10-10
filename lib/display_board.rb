a073c6fe21b71f3ad866ec0dbbaee9beb1c56653
David Reep <davereep@gmail.com>
2017-10-10 19:39:10 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,12 +1,17 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-def display_board
-  cell = "   "
-  row = cell + "|" + cell + "|" + cell
 
-  puts row
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " ", ]
+
+def display_board(board)
+  cell = "  "
+  row1 = cell + board[0] + "|" + cell + board[1] + "|" + cell + board[2]
+  row2 = cell + board[3] + "|" + cell + board[4] + "|" + cell + board[5]
+  row3 = cell + board[6] + "|" + cell + board[7] + "|" + cell + board[8]
+
+  puts row1
   puts "-----------"
-  puts row
+  puts row2
   puts "-----------"
-  puts row
+  puts row3
 end
